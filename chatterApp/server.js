0aa43ead8c2e47e3c8a0ff7ed8804ada9ea9f85e
Ruben <ruben.alexanyan1@gmail.com>
2020-05-10 00:52:51 +0400

    Added get and post

--- chatterApp/server.js
+++ chatterApp/server.js
@@ -5,6 +5,19 @@ const app = express();
 
 app.use(express.static(__dirname));
 
+let messages = [
+  { name: "Rub", message: "Hello" },
+  { name: "Nima", message: "Hello Everyone" },
+];
+
+app.get("/messages", (req, res) => {
+  res.send(messages);
+});
+
+app.post("/messages", (req, res) => {
+  res.sendStatus(200);
+});
+
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
